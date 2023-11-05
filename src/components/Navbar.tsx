498b1ce82833103323cc379a31891c3110b94f4a
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-11-05 20:32:16 +0200

    Create NavBar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-900 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a className="text-white text-2xl font-bold" href="/">Car Wash</a>
+        <div className="space-x-4">
+          <a className="text-white hover:underline" href="/">Dashboard</a>
+          <a className="text-white hover:underline" href="/library">Library</a>
+          <a className="text-white hover:underline" href="/form">Form</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
