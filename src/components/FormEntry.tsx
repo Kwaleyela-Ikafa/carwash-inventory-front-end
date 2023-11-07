d3235acbcb4419f7177d2ae8f2d85bd29588dc5e
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-11-07 18:02:11 +0200

    Calculate share percentage

--- src/components/FormEntry.tsx
+++ src/components/FormEntry.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 interface FormEntryData {
@@ -6,11 +6,14 @@ interface FormEntryData {
     contact: string;
     service: string;
     vehicle: string;
+    registration: string;
     cleaner: string;
     share: string;
+    share_percentage: number;
     price: number;
     p_m: string;
     cashier: string;
+    comments: string;
 }
   
 
@@ -19,19 +22,30 @@ interface FormEntryData {
     contact: "",
     service: "",
     vehicle: "",
+    registration: "",
     cleaner: "",
     share: "",
+    share_percentage: 0,
     price: 0,
     p_m: "",
     cashier: "",
+    comments: "",
   };
   
   const FormEntry: React.FC = () => {
     const [formData, setFormData] = useState<FormEntryData>(initialFormData);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setFormData((prevData) => ({ ...prevData, [name]: value }));
+  useEffect(() => {
+    // Calculate the share when the Price or Share Percentage changes
+    const price = parseFloat(formData.price.toString());
+    const sharePercentage = parseFloat(formData.share_percentage.toString());
+    const calculatedShare = (price * sharePercentage) / 100;
+    setFormData((prevData) => ({ ...prevData, share: calculatedShare.toFixed(2).toString() }));
+  }, [formData.price, formData.share_percentage]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -95,13 +109,23 @@ interface FormEntryData {
           <label htmlFor="service" className="block text-gray-700 text-sm font-bold mb-2">
             Service:
           </label>
-          <input
-            type="text"
+          <select
+            // type="text"
             name="service"
             value={formData.service}
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+          >
+            <option value=" ">Select Service</option>
+            <option value="EC">External cleaning (EC)</option>
+            <option value="IC">Internal cleaning (IC)</option>
+            <option value="IEC">Internal & External cleaning (IEC)</option>
+            <option value="CC">Carpet cleaning (CC)</option>
+            <option value="TC">Tire cleaning (TC)</option>
+            <option value="EW"> Engine Wash (EW)</option>
+            <option value="FV">Full Valet (FV)</option>
+            <option value="Other">Other</option>
+          </select>
         </div>
 
         {/* Vehicle */}
@@ -113,6 +137,20 @@ interface FormEntryData {
             type="text"
             name="vehicle"
             value={formData.vehicle}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        {/* Registration */}
+        <div className="mt-4">
+          <label htmlFor="registration" className="block text-gray-700 text-sm font-bold mb-2">
+            REG:
+          </label>
+          <input
+            type="text"
+            name="registration"
+            value={formData.registration}
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
@@ -132,19 +170,6 @@ interface FormEntryData {
           />
         </div>
 
-        {/* Share */}
-        <div className="mt-4">
-          <label htmlFor="share" className="block text-gray-700 text-sm font-bold mb-2">
-            Share:
-          </label>
-          <input
-            type="text"
-            name="share"
-            value={formData.share}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
 
         {/* Price */}
         <div className="mt-4">
@@ -160,18 +185,51 @@ interface FormEntryData {
           />
         </div>
 
+        {/* Share Percentage*/}
+        <div className="mt-4">
+          <label htmlFor="share_percentage" className="block text-gray-700 text-sm font-bold mb-2">
+            Share Percentage:
+          </label>
+          <input
+            type="number"
+            name="share_percentage"
+            value={formData.share_percentage}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        {/* Share */}
+        <div className="mt-4">
+          <label htmlFor="share" className="block text-gray-700 text-sm font-bold mb-2">
+            Share:
+          </label>
+          <input
+            type="text"
+            name="share"
+            value={formData.share}
+            onChange={handleChange}
+            readOnly 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
         {/* P M */}
         <div className="mt-4">
           <label htmlFor="p_m" className="block text-gray-700 text-sm font-bold mb-2">
             P M:
           </label>
-          <input
-            type="text"
+          <select
+            // type="text"
             name="p_m"
             value={formData.p_m}
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+          >
+            <option value=" ">Select Payment Method</option>
+            <option value="AM">AM</option>
+            <option value="CH">CH</option>
+          </select>
         </div>
 
         {/* Cashier */}
@@ -183,6 +241,20 @@ interface FormEntryData {
             type="text"
             name="cashier"
             value={formData.cashier}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        {/* Commments */}
+        <div className="mt-4">
+          <label htmlFor="comments" className="block text-gray-700 text-sm font-bold mb-2">
+            Comments:
+          </label>
+          <input
+            type="text"
+            name="comments"
+            value={formData.comments}
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
