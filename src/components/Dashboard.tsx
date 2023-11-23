7722e798c6194d4daa2bdcc723b670c4c51d4adf
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-11-23 19:13:13 +0200

    add new charts

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import RadarChart from "../charts/RadarChart";
 import ColumnChart from "../charts/ColumnChart";
+import ServiceChart from "../charts/ServiceChart";
+import PaymentMethodChart from "../charts/PaymentMethodChart";
 
 const Dashboard = () => {
   return (
     <div className="container mx-auto mt-8">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      {/* Add your dashboard content here */}
+
       <div className="flex justify-evenly items-start gap-4 pl-2 pr-2 mb-4">
         <RadarChart />
         <ColumnChart />
+      </div>
+      <div className="flex justify-evenly items-start gap-4 pl-2 pr-2 mb-4">
+        <ServiceChart />
+        <PaymentMethodChart />
       </div>
     </div>
   );
